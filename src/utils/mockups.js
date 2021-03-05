702799d88f5eabd056a9cf9bce39a99b3ee7ebd1
Eduardo <eduardojoaoaraujo@gmail.com>
2021-03-05 18:14:17 +0000

    Changed images and fixed window error on build

--- src/utils/mockups.js
+++ src/utils/mockups.js
@@ -1,26 +1,26 @@
 export const mockups = {
   firstSection: {
     dark:
-      'https://res.cloudinary.com/trckr/image/upload/v1614194708/Landing/homehabits_r0xhzh.png',
+      'https://res.cloudinary.com/trckr/image/upload/q_auto/f_auto/v1614194708/Landing/homehabits_r0xhzh.png',
     light:
-      'https://res.cloudinary.com/trckr/image/upload/v1614194707/Landing/homehabits-l_ktocmf.png'
+      'https://res.cloudinary.com/trckr/image/upload/q_auto/f_auto/v1614194707/Landing/homehabits-l_ktocmf.png'
   },
   banner: {
     dark:
-      'https://res.cloudinary.com/trckr/image/upload/v1614194707/Landing/signup_suqrfk.png',
+      'https://res.cloudinary.com/trckr/image/upload/q_auto/f_auto/v1614194707/Landing/signup_suqrfk.png',
     light:
-      'https://res.cloudinary.com/trckr/image/upload/v1614194707/Landing/signup-l_rcraxa.png'
+      'https://res.cloudinary.com/trckr/image/upload/q_auto/f_auto/v1614194707/Landing/signup-l_rcraxa.png'
   },
   thirdSection: {
     dark:
-      'https://res.cloudinary.com/trckr/image/upload/v1614194707/Landing/habitsStats_saiini.png',
+      'https://res.cloudinary.com/trckr/image/upload/q_auto/f_auto/v1614194707/Landing/habitsStats_saiini.png',
     light:
-      'https://res.cloudinary.com/trckr/image/upload/v1614194707/Landing/habitsStats-l_i0cjni.png'
+      'https://res.cloudinary.com/trckr/image/upload/q_auto/f_auto/v1614194707/Landing/habitsStats-l_i0cjni.png'
   },
   fourthSection: {
     dark:
-      'https://res.cloudinary.com/trckr/image/upload/v1614194858/Landing/addTask_kwuik3.png',
+      'https://res.cloudinary.com/trckr/image/upload/q_auto/f_auto/v1614194858/Landing/addTask_kwuik3.png',
     light:
-      'https://res.cloudinary.com/trckr/image/upload/v1614194857/Landing/addTask-l_klbsjy.png'
+      'https://res.cloudinary.com/trckr/image/upload/q_auto/f_auto/v1614194857/Landing/addTask-l_klbsjy.png'
   }
 }
--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -3,7 +3,8 @@ import { useState } from 'react'
 export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      const item = window.localStorage.getItem(key)
+      const item =
+        typeof window !== `undefined` ? window.localStorage.getItem(key) : ''
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
       console.log(error)
@@ -16,7 +17,9 @@ export const useLocalStorage = (key, initialValue) => {
       const valueToStore =
         value instanceof Function ? value(storedValue) : value
       setStoredValue(valueToStore)
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      if (typeof window !== `undefined`) {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      }
     } catch (error) {
       console.log(error)
     }
