5af145a76f68d7a8d41667078f9c00063ae23aa2
Eduardo <eduardojoaoaraujo@gmail.com>
2021-03-28 15:37:43 +0100

    Removed unused usestate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Topbar from '../components/Topbar'
 import FirstSection from '../components/FirstSection'
 import SecondSection from '../components/SecondSection'
